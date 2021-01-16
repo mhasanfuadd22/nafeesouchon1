64c5a0cbeec23688376e0c2925f7116f30fadc6d
mhasanfuadd22 <76463319+mhasanfuadd22@users.noreply.github.com>
2021-01-16 08:59:00 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// untuk menambahkan tinggal copy yang ada terus paste di bawahnya, 
+// ganti kode negara sesuai countrycode.org
+
+$linkOffer = array( 
+    "ALL" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1161370&s3=xnxx&click_id=MACMOOS&j1=1&j3=1", // Yang ini jangan dihapus, sebagai default
+    "IN" => "https://www.youtube.com/",
+    "ID" => "https://www.google.com/",
+    "BR" => "https://www.google.com/",
+    "AR" => "https://www.google.com/",
+    "FR" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1194120&s3=porn&click_id=MACMOOS21&j1=1&j3=1",
+);
