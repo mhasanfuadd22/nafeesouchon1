360dc8c01688ce354f5aa34c4df3ef7537107056
mhasanfuadd22 <76463319+mhasanfuadd22@users.noreply.github.com>
2021-01-22 14:11:57 +0700

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,12 @@ $linkOffer = array(
     "ALL" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1161370&s3=xnxx&click_id=MACMOOS&j1=1&j3=1", // Yang ini jangan dihapus, sebagai default
     "IN" => "https://www.youtube.com/",
     "ID" => "https://www.google.com/",
-    "BR" => "https://www.google.com/",
-    "AR" => "https://www.google.com/",
     "FR" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1194120&s3=porn&click_id=MACMOOS21&j1=1&j3=1",
+    "DE" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1194120&s3=porn&click_id=MACMOOS21&j1=1&j3=1",
+    "CH" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1194120&s3=porn&click_id=MACMOOS21&j1=1&j3=1",
+    "AT" => "https://vozkn.shewantyou.com/c/da57dc555e50572d?s1=74105&s2=1194120&s3=porn&click_id=MACMOOS21&j1=1&j3=1",
+    "AR" => "https://www.google.com/",
+    "MA" => "https://www.google.com/",
+    "BR" => "https://www.youtube.com/",
+    
 );
